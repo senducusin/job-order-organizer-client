a38791d1023fa109bd29f29e85ebf1c77ce6011d
Jansen Ducusin <senducusin@gmail.com>
2022-07-10 03:16:21 +0800

    Add warning modal on import

---
+++ src/components/SideMenu/Alert.js
@@ -0,0 +1,32 @@
+import Modal from "../Views/Modal";
+import classes from "./Alert.module.css";
+
+const Alert = (props) => {
+  return (
+    <Modal onClose={props.onClose}>
+      <h1 className={classes.content}>Warning!</h1>
+      <h3 className={classes.content}>
+        Importing a file will remove all existing saved accounts and job orders.
+      </h3>
+      <h3 className={classes.content}>Do you want to proceed?</h3>
+
+      <div className={classes["button-container"]}>
+        <button
+          onClick={props.onClose}
+          className={`${classes.button} ${classes.cancel}`}
+        >
+          <h2>Cancel</h2>
+        </button>
+        <div className={classes.spacer}></div>
+        <button
+          onClick={props.onImport}
+          className={`${classes.button} ${classes.proceed}`}
+        >
+          <h2>Proceed</h2>
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Alert;
--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,18 +1,39 @@
+import { Fragment, useState } from "react";
+import Alert from "./Alert";
 import classes from "./SideMenu.module.css";
 
 const SideMenu = (props) => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  const importHandler = () => {
+    props.import();
+    setCartIsShown(false);
+  };
+
   return (
-    <div className={classes.sidemenu}>
-      {/* <div className={classes.spacer}></div> */}
-      <div className={classes.container}>
-        <button className={classes.import} onClick={props.import}>
-          Import
-        </button>
-        <button className={classes.export} onClick={props.export}>
-          Export
-        </button>
+    <Fragment>
+      {cartIsShown && (
+        <Alert onClose={hideCartHandler} onImport={importHandler} />
+      )}
+      <div className={classes.sidemenu}>
+        <div className={classes.container}>
+          <button className={classes.import} onClick={showCartHandler}>
+            Import
+          </button>
+          <button className={classes.export} onClick={props.export}>
+            Export
+          </button>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
